61176307eda57105357e504000293ae6d43171c8
Dyego Soriano <dyegodf2@gmail.com>
2020-08-04 20:50:53 -0300

    listando aulas

--- server/src/controller/ClassesController.ts
+++ server/src/controller/ClassesController.ts
@@ -68,6 +68,46 @@ class ClassesController {
         .json({ error: `Sorry! There was an error on our server.` })
     }
   }
+
+  async index(request: Request, response: Response) {
+    const filters = request.query
+
+    const week_day = filters.week_day as string
+    const subject = filters.subject as string
+    const time = filters.time as string
+
+    if (!week_day || !subject || !time) {
+      return response
+        .status(400)
+        .json({ error: 'Missing filters to search classes' })
+    }
+
+    const timeInMinutes = convertHoursToMinutes(time)
+
+    try {
+      // Listagem de dados
+      const classes = await db('classes')
+        .whereExists(function () {
+          this.select('class_schedule.*')
+            .from('class_schedule')
+            .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+            .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+            .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+            .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+        })
+        .where('classes.subject', '=', subject)
+        .join('users', 'classes.user_id', '=', 'users.id')
+        .select(['classes.*', 'users.*'])
+
+      return response.json(classes)
+    } catch (error) {
+      console.log(`error.message >>> ${error.message} <<<`)
+
+      return response
+        .status(500)
+        .json({ error: `Sorry! There was an error on our server.` })
+    }
+  }
 }
 
 export default new ClassesController()
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -5,5 +5,6 @@ import ClassesController from './controller/ClassesController'
 const routes = new Router()
 
 routes.post('/classes', ClassesController.store)
+routes.get('/classes', ClassesController.index)
 
 export default routes
